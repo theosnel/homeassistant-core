7fd999ccc4114068c19c1eb3dcfd1a65a36fc3d8
Theo Snelleman <theo@zolder.io>
2024-04-13 15:41:49 +0000

    Add coordinator and entity

--- homeassistant/components/xsense/__init__.py
+++ homeassistant/components/xsense/__init__.py
@@ -1,26 +1,23 @@
 """The X-Sense Home Security integration."""
 from __future__ import annotations
 
-from xsense import AsyncXSense
-
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, Platform
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
+from .coordinator import XSenseDataUpdateCoordinator
 
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up X-Sense Home Security from a config entry."""
-    email = entry.data[CONF_EMAIL]
-    password = entry.data[CONF_PASSWORD]
-    api = AsyncXSense()
-    await api.init()
-    await api.login(email, password)
+    coordinator = XSenseDataUpdateCoordinator(hass, entry)
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = api
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.async_on_unload(entry.add_update_listener(update_listener))
