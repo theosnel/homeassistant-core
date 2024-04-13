fe742c2c5114063bf6f2861fcddc44a0f1340970
Theo Snelleman <theo@zolder.io>
2024-04-13 15:41:49 +0000

    Add coordinator and entity

--- homeassistant/components/xsense/sensor.py
+++ homeassistant/components/xsense/sensor.py
@@ -1,20 +1,74 @@
 """Support for xsense sensors."""
 from __future__ import annotations
 
-from xsense import device
+from collections.abc import Callable
+from dataclasses import dataclass
+
+from xsense.device import Device
 
 from homeassistant import config_entries
 from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorEntity,
+    SensorEntityDescription,
     SensorStateClass,
 )
-from homeassistant.const import CONF_EMAIL, SIGNAL_STRENGTH_DECIBELS
+from homeassistant.const import (
+    CONCENTRATION_PARTS_PER_MILLION,
+    PERCENTAGE,
+    UnitOfTemperature,
+)
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import StateType
 
-from .const import DOMAIN, MANUFACTURER
+from .const import DOMAIN
+from .coordinator import XSenseDataUpdateCoordinator
+from .entity import XSenseEntity
+
+
+@dataclass(kw_only=True, frozen=True)
+class XSenseSensorEntityDescription(SensorEntityDescription):
+    """Describes XSense sensor entity."""
+
+    exists_fn: Callable[[Device], bool] = lambda _: True
+    value_fn: Callable[[Device], StateType]
+
+
+SENSORS: tuple[XSenseSensorEntityDescription, ...] = (
+    XSenseSensorEntityDescription(
+        key="co",
+        native_unit_of_measurement=CONCENTRATION_PARTS_PER_MILLION,
+        device_class=SensorDeviceClass.CO,
+        state_class=SensorStateClass.MEASUREMENT,
+        value_fn=lambda device: device.data["coPpm"],
+        exists_fn=lambda device: "coPpm" in device.data,
+    ),
+    XSenseSensorEntityDescription(
+        key="temperature",
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+        device_class=SensorDeviceClass.TEMPERATURE,
+        state_class=SensorStateClass.MEASUREMENT,
+        value_fn=lambda device: device.data["temperature"],
+        exists_fn=lambda device: "temperature" in device.data,
+    ),
+    XSenseSensorEntityDescription(
+        key="humidity",
+        native_unit_of_measurement=PERCENTAGE,
+        device_class=SensorDeviceClass.HUMIDITY,
+        state_class=SensorStateClass.MEASUREMENT,
+        value_fn=lambda device: device.data["humidity"],
+        exists_fn=lambda device: "humidity" in device.data,
+    ),
+    XSenseSensorEntityDescription(
+        key="battery",
+        native_unit_of_measurement=PERCENTAGE,
+        device_class=SensorDeviceClass.BATTERY,
+        state_class=SensorStateClass.MEASUREMENT,
+        value_fn=lambda device: (int(device.data["batInfo"]) * 100) / 3,
+        exists_fn=lambda device: "batInfo" in device.data,
+    ),
+)
 
 
 async def async_setup_entry(
@@ -23,35 +77,38 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the xsense sensor entry."""
-    devices = []
-    api = hass.data[DOMAIN][entry.entry_id]
-    await api.load_all()
-    for _, h in api.houses.items():
-        for _, s in h.stations.items():
-            await api.get_state(s)
-            for _, d in s.devices.items():
-                devices.append(XSenseSensor(d, entry))
+    devices: list[Device] = []
+    coordinator: XSenseDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+
+    for k, dev in coordinator.data.items():
+        devices.extend(
+            XSenseSensorEntity(coordinator, k, description)
+            for description in SENSORS
+            if description.exists_fn(dev)
+        )
 
     async_add_entities(devices)
 
 
-class XSenseSensor(SensorEntity):
+class XSenseSensorEntity(XSenseEntity, SensorEntity):
     """Representation of a xsense device."""
 
-    _attr_native_unit_of_measurement = SIGNAL_STRENGTH_DECIBELS
-    _attr_device_class = SensorDeviceClass.SIGNAL_STRENGTH
-    _attr_state_class = SensorStateClass.MEASUREMENT
+    entity_description: XSenseSensorEntityDescription
 
-    def __init__(self, dev: device, entry) -> None:
-        """Initiate xsense device."""
-        self._attr_unique_id = dev.sn
-        self._attr_name = dev.name
-        self._attr_native_value = dev.rf_level
+    def __init__(
+        self,
+        coordinator: XSenseDataUpdateCoordinator,
+        device_id: str,
+        entity_description: XSenseSensorEntityDescription,
+    ) -> None:
+        """Set up the instance."""
+        self.entity_description = entity_description
+        self._attr_available = False  # This overrides the default
 
-        self._attr_device_info: DeviceInfo = DeviceInfo(
-            name=dev.name,
-            manufacturer=MANUFACTURER,
-            model=dev.device_type,
-            identifiers={(DOMAIN, dev.sn)},
-            via_device=(DOMAIN, entry.data[CONF_EMAIL]),
-        )
+        super().__init__(coordinator, device_id)
+
+    @property
+    def native_value(self) -> str | int | float | None:
+        """Return the state of the sensor."""
+        device = self.coordinator.data[self._dev_id]
+        return self.entity_description.value_fn(device)
