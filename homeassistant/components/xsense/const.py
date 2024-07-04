bd386e5c76d2d08ebecb549815e4f30a923f5a15
Theo Snelleman <theo@zolder.io>
2024-07-04 06:49:28 +0000

    Add support for realtime updates with MQTT connections

--- homeassistant/components/xsense/const.py
+++ homeassistant/components/xsense/const.py
@@ -1,4 +1,5 @@
 """Constants for the X-Sense Home Security integration."""
+
 import logging
 
 DOMAIN = "xsense"
@@ -6,6 +7,7 @@ MANUFACTURER = "X-Sense"
 COORDINATOR = "coordinator"
 
 DEFAULT_SCAN_INTERVAL = 60
+POLL_INTERVAL_MIN = 1
 
 LOGGER = logging.getLogger(__package__)
 
