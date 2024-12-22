a95a3b793135e9a63fdb1911aff3bf86697fcdd3
Theo Snelleman <theo@zolder.io>
2024-12-22 15:19:41 +0000

    Do not set parent to account

--- homeassistant/components/xsense/entity.py
+++ homeassistant/components/xsense/entity.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from xsense.entity import Entity
 
-from homeassistant.const import ATTR_VIA_DEVICE, CONF_EMAIL
+from homeassistant.const import ATTR_VIA_DEVICE
 from homeassistant.helpers.device_registry import (
     CONNECTION_BLUETOOTH,
     CONNECTION_NETWORK_MAC,
@@ -54,10 +54,7 @@ class XSenseEntity(CoordinatorEntity[XSenseDataUpdateCoordinator]):
         )
         if station_id:
             parent = (DOMAIN, station_id)
-        else:
-            parent = (DOMAIN, coordinator.entry.data[CONF_EMAIL])
-
-        self._attr_device_info.update({ATTR_VIA_DEVICE: parent})
+            self._attr_device_info.update({ATTR_VIA_DEVICE: parent})
 
     async def async_added_to_hass(self) -> None:
         """Subscribe to updates."""
