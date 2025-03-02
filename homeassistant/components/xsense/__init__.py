03cc76c07e4951ef780e91f9b9edb51b688f757a
Theo Snelleman <theo@snelleman.net>
2025-03-02 21:29:31 +0000

    Remove bronze indication, thats a lie

--- homeassistant/components/xsense/__init__.py
+++ homeassistant/components/xsense/__init__.py
@@ -18,8 +18,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await coordinator.async_config_entry_first_refresh()
 
-    entry.runtime_data = "todo"
-
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
